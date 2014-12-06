a817d6962ca3fa3ec0123ebd83d96b624bbd78a1
david karapetyan <dkarapetyan@scriptcrafty.com>
2014-12-06 18:26:58 -0500

    boilerplate for examples

--- interactions.ts
+++ interactions.ts
@@ -1,8 +1,11 @@
 var examples : { [n : string] : string } = {
-  'anonymous-function': "(fun (x y) (+ x y))",
-  'function-application': "((fun (x y) (+ x y)) 1 2)",
-  'let-in': "(let (add (fun (x y) (+ x y))) (add 1 2))",
-  'pattern-matching': "(let (map (fun (f l) (match l ([] [] (cons x xs) (cons (f x) (map f xs)))))) (map (fun (x) x) [1, 2, 3]))"
+  'anonymous function': "(fun (x y) (+ x y))",
+  'function application': "((fun (x y) (+ x y)) 1 2)",
+  'factorial': "(let (factorial (fun (x) (if (less-than 0 x) (* x (factorial (- x 1))) 1))) (factorial 3))",
+  'let in': "(let (add (fun (x y) (+ x y))) (add 1 2))",
+  'pattern matching': "(let (map (fun (f l) (match l ([] [] (cons x xs) (cons (f x) (map f xs)))))) (map (fun (x) x) [1, 2, 3]))",
+  'tuple': "<1, 2, 3>",
+  'list': "[1, 2, 3]"
 }
 
 function set_example(name : string) : string {
@@ -35,4 +38,16 @@ function parse_input() {
   var ast = G.parse(lexemes);
   ast_area().value = JSON.stringify(ast);
   return ast;
+}
+
+function instantiate_examples() {
+  var div = document.querySelector('#buttons');
+  for (var k in examples) {
+    var example : string = examples[k];
+    var element = document.createElement('input');
+    element.type = 'button';
+    element.value = k;
+    element.onclick = (function(button_name : string) { return function() { set_example(button_name); }; })(k);
+    div.appendChild(element);
+  }
 }
