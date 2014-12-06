b70e15c65106ea6b5632db0d6525e3fdeea9acdd
david karapetyan <dkarapetyan@scriptcrafty.com>
2014-12-06 14:18:31 -0500

    examples

--- interactions.ts
+++ interactions.ts
@@ -1,3 +1,15 @@
+var examples : { [n : string] : string } = {
+  'anonymous-function': "(fun (x y) (+ x y))",
+  'function-application': "((fun (x y) (+ x y)) 1 2)",
+  'let-in': "(let (add (fun (x y) (+ x y))) (add 1 2))",
+  'pattern-matching': "(let (map (fun (f l) (match l (() '() (cons x xs) (cons (f x) (map f xs)))))) (map (fun (x) x) (cons 1 (cons 2 '()))))"
+}
+
+function set_example(name : string) : string {
+  input_area().value = examples[name];
+  return examples[name];
+}
+
 function input_area() : HTMLTextAreaElement {
   return (<HTMLTextAreaElement>document.querySelector('#input'));
 }
