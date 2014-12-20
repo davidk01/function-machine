6a8b3465944cebe3c3dcb14d87af0746c0f9c78c
david karapetyan <dkarapetyan@scriptcrafty.com>
2014-12-19 19:11:40 -0500

    function to parenthesize

--- parser.ts
+++ parser.ts
@@ -11,7 +11,13 @@ class G {
     return Parser.m(x => x.characters === w);
   }
 
+  // Comes up often enough.
   static delayed_sexpr : Parser = Parser.delay(x => G.s_expr);
+
+  // Another common operation: ( p )
+  static parenthesize(p : Parser) : Parser {
+    return G.lparen.then(p).then(G.rparen);
+  }
 
   // Number token.
   static num : Parser = Parser.m(x => x.type === TokenType.NUMBER).transformer(
@@ -28,42 +34,41 @@ class G {
   static rparen : Parser = Parser.m(x => x.type === TokenType.RPAREN);
 
   // (sym sym ... sym).
-  static symbol_list : Parser = G.lparen.then(G.symb.many()).then(G.rparen).transformer((x) : Array<Symbol> => {
+  static symbol_list : Parser = G.parenthesize(G.symb.many()).transformer((x) : Array<Symbol> => {
     return x[1];
   });
 
   // (fun (sym sym sym) s-expr).
-  static anonymous_func : Parser = G.lparen.then(G.word('fun')).then(G.symbol_list).then(
-    G.delayed_sexpr).then(G.rparen).transformer((x) : AnonymousFunction => {
+  static anonymous_func : Parser = G.parenthesize(G.word('fun').then(G.symbol_list).then(
+    G.delayed_sexpr)).transformer((x) : AnonymousFunction => {
       var args = x[2];
       var body = x[3];
       return new AnonymousFunction(args, body, {arg_count: args.length});
-    });
+  });
 
   // ((fun ...) s-expr*).
-  static func_application : Parser = G.lparen.then(G.anonymous_func).then(G.delayed_sexpr.zero_or_more()).then(
-    G.rparen).transformer((x) : FunctionApplication => {
+  static func_application : Parser = G.parenthesize(G.anonymous_func.then(G.delayed_sexpr.zero_or_more()).then(
+    G.rparen)).transformer((x) : FunctionApplication => {
       var func = x[1];
       var args = x[2];
       return new FunctionApplication(func, args, {arg_count: func.attrs.arg_count - args.length});
-    });
+  });
 
   // (applied-func s-expr*).
-  static closure : Parser = G.lparen.then(G.func_application).then(G.delayed_sexpr.zero_or_more()).then(
-    G.rparen).transformer((x) : ClosureApplication => {
+  static closure : Parser = G.parenthesize(G.func_application.then(G.delayed_sexpr.zero_or_more())).transformer((x) : ClosureApplication => {
       var applied_func = x[1];
       var args = x[2];
       return new ClosureApplication(applied_func, args, {arg_count: applied_func.attrs.arg_count - args.length});
-    });
+  });
 
   // (if s-expr s-expr s-expr).
-  static if_expression : Parser = G.lparen.then(G.word('if')).then(G.delayed_sexpr).then(
-    G.delayed_sexpr).then(G.delayed_sexpr).then(G.rparen).transformer((x) : IfExpression => {
+  static if_expression : Parser = G.parenthesize(G.word('if').then(G.delayed_sexpr).then(
+    G.delayed_sexpr).then(G.delayed_sexpr)).transformer((x) : IfExpression => {
         var test = x[2];
         var true_branch = x[3];
         var false_branch = x[4];
         return new IfExpression(test, true_branch, false_branch, {});
-      });
+  });
 
   // < Left angle bracket.
   static langle : Parser = Parser.m(x => x.type === TokenType.LANGLE);
@@ -81,7 +86,7 @@ class G {
   static tuple : Parser = G.langle.then(G.delayed_sexpr).then(G.delayed_sexpr.zero_or_more()).then(
     G.rangle).transformer((x : Array<any>) : Tuple => {
       return new Tuple([x[1]].concat(x[2]), {});
-    });
+  });
 
   // TODO: Figure this out. Currently a wildcard.
   static pattern : Parser = Parser.delay(x => G.s_expr);
@@ -91,21 +96,21 @@ class G {
       var variable = x[1];
       var value = x[2];
       return new BindingPair(variable, value, {});
-    });
+  });
 
   // (let <var s-expr>* s-expr).
-  static let_expression : Parser = G.lparen.then(G.word('let')).then(G.binding_pair.many()).then(
-    G.delayed_sexpr).then(G.rparen).transformer((x) : LetExpressions => {
+  static let_expression : Parser = G.parenthesize(G.word('let').then(G.binding_pair.many()).then(
+    G.delayed_sexpr)).transformer((x) : LetExpressions => {
       var binding_pairs = x[2];
       var body = x[3];
       return new LetExpressions(binding_pairs, body, {});
-    });
+  });
     
   // List: [s-expr, ..., s-expr].
   static non_empty_data_list : Parser = G.lbracket.then(G.delayed_sexpr).then(G.delayed_sexpr.zero_or_more()).then(
     G.rbracket).transformer((x : Array<any>) : List => {
       return new List([x[1]].concat(x[2]), {});
-    });
+  });
 
   // Empty list: [].
   static empty_data_list : Parser = G.lbracket.then(G.rbracket).transformer((x : Array<any>) : List => {
@@ -118,10 +123,9 @@ class G {
       G.let_expression).or(G.closure).or(G.symb);
 
   // Non-empty list: (atomic s-expr*). This is just here while I work out the syntax.
-  static list : Parser = G.lparen.then(G.atomic).then(G.delayed_sexpr.zero_or_more()).then(
-    G.rparen).transformer((x) : Array<ASTNode> => {
+  static list : Parser = G.parenthesize(G.atomic.then(G.delayed_sexpr.zero_or_more())).transformer((x) : Array<ASTNode> => {
       return [x[1]].concat(x[2]);
-    });
+  });
 
   // s-expr: atomic | empty list | non-empty list.
   static s_expr : Parser = G.atomic.or(G.list);
